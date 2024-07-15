0502127e017730061985dd80063b91a826f5243c
Mariano Lotero Rolon <marianonehuenlr@gmail.com>
2024-07-15 18:20:11 -0300

    + product controller

---
+++ controllers/productController.js
@@ -0,0 +1,25 @@
+import Product from '../models/Product.js'
+import jwt from 'jsonwebtoken'
+import 'dotenv/config'
+
+const {JWT_SECRET} = process.env
+
+export async function getAll (req, res) {
+    //TODO
+}
+
+export async function getById (req, res) {
+    //TODO
+}
+
+export async function add (req, res) {
+    //TODO
+}
+
+export async function update (req, res) {
+    //TODO
+}
+
+export async function deleteProduct (req, res) {
+    //TODO
+}
