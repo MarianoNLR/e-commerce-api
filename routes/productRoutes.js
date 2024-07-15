8bf5179931423bb19c6e61a6e1d669eb8e67413d
Mariano Lotero Rolon <marianonehuenlr@gmail.com>
2024-07-15 18:19:48 -0300

    + product routes

---
+++ routes/productRoutes.js
@@ -0,0 +1,12 @@
+import express from 'express'
+import {add, deleteProduct, getAll, getById, update } from '../controllers/productController.js'
+
+const productRouter = express.Router()
+
+productRouter.post('/', add)
+productRouter.delete('/:id', deleteProduct)
+productRouter.get('/', getAll)
+productRouter.get('/:id', getById)
+productRouter.patch('/:id', update)
+
+export default productRouter
