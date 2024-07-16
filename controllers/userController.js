b8d9e6a72299d629f3410613f643b64224e4dc8e
Mariano Lotero Rolon <marianonehuenlr@gmail.com>
2024-07-16 18:26:48 -0300

    login method

--- controllers/userController.js
+++ controllers/userController.js
@@ -7,14 +7,39 @@ import 'dotenv/config'
 const { JWT_SECRET } = process.env
 
 export async function login (req, res) {
-    //TODO Login
+    const { username, password } = req.body
+
+    try {
+        const user = await User.findOne({ username })
+
+        if (!user) {
+            return res.status(401).json({message: 'Username or password incorrect.'})
+        }
+
+        const passwordMatch = await bcrypt.compare(password, user.password)
+
+        if (!passwordMatch) {
+            return res.status(401).json({message: 'Username or password incorrect.'})
+        }
+
+        const token = jwt.sign({userId: user._id, username: user.username, password: user.password}, JWT_SECRET)
+
+        res.cookie('access_token', token, {
+            httpOnlu: true,
+            secure: false
+        })
+
+        return res.status(200).json({ user, token })
+    } catch (error) {
+        return res.status(500).json({error})
+    }
 }
 
 export async function register (req, res) {
     const {username, password, confirmPassword} = req.body
 
     try {
-        const userExists = User.findOne({ username })
+        const userExists = await User.findOne({ username })
 
         if (userExists) return res.status(401).json({ message: 'Username already existes.' })
         
