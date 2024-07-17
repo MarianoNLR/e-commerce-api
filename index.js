faacf0ce6b6dea2e17661c9a14b50b6715a894d0
Mariano Lotero Rolon <marianonehuenlr@gmail.com>
2024-07-17 17:55:32 -0300

    body parser and user router

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 import express from 'express'
+import userRouter from './routes/userRoutes.js'
+import bodyParser from 'body-parser'
+import './mongo.js'
 
 const app = express()
 
 const PORT = process.env.PORT ?? 3000
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.use('/users', userRouter)
 
 app.get('/', (req, res) => {
   res.send('<h1>Hello World!!</h1>')
