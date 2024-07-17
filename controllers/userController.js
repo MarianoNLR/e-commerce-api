6e3a14f49bce2e447afcf0aa9e614fb92dc1074c
Mariano Lotero Rolon <marianonehuenlr@gmail.com>
2024-07-17 17:16:18 -0300

    format and get al users and by id method

--- controllers/userController.js
+++ controllers/userController.js
@@ -7,61 +7,82 @@ import 'dotenv/config'
 const { JWT_SECRET } = process.env
 
 export async function login (req, res) {
-    const { username, password } = req.body
+  const { username, password } = req.body
 
-    try {
-        const user = await User.findOne({ username })
+  try {
+    const user = await User.findOne({ username })
 
-        if (!user) {
-            return res.status(401).json({message: 'Username or password incorrect.'})
-        }
-
-        const passwordMatch = await bcrypt.compare(password, user.password)
-
-        if (!passwordMatch) {
-            return res.status(401).json({message: 'Username or password incorrect.'})
-        }
-
-        const token = jwt.sign({userId: user._id, username: user.username, password: user.password}, JWT_SECRET)
-
-        res.cookie('access_token', token, {
-            httpOnlu: true,
-            secure: false
-        })
-
-        return res.status(200).json({ user, token })
-    } catch (error) {
-        return res.status(500).json({error})
+    if (!user) {
+      return res.status(401).json({ message: 'Username or password incorrect.' })
     }
+
+    const passwordMatch = await bcrypt.compare(password, user.password)
+
+    if (!passwordMatch) {
+      return res.status(401).json({ message: 'Username or password incorrect.' })
+    }
+
+    const token = jwt.sign({ userId: user._id, username: user.username, password: user.password }, JWT_SECRET)
+
+    res.cookie('access_token', token, {
+      httpOnlu: true,
+      secure: false
+    })
+
+    return res.status(200).json({ user, token })
+  } catch (error) {
+    return res.status(500).json({ error })
+  }
 }
 
 export async function register (req, res) {
-    const {username, password, confirmPassword} = req.body
+  const { username, password, confirmPassword } = req.body
 
-    try {
-        const userExists = await User.findOne({ username })
+  if (password !== confirmPassword) return res.status(401).json({ message: 'Passwords must match.' })
 
-        if (userExists) return res.status(401).json({ message: 'Username already existes.' })
-        
-            const hashedPassword = await bcrypt.hash(password, 10)
-            const newUser = new User({ username: username, password: hashedPassword })
+  try {
+    const userExists = await User.findOne({ username })
 
-            await newUser.save()
+    if (userExists) return res.status(401).json({ message: 'Username already existes.' })
 
-            return res.json({username, password: hashedPassword})
-    } catch (error) {
-        res.status(500).send(error)
-    }
+    const hashedPassword = await bcrypt.hash(password, 10)
+    const newUser = new User({ username, password: hashedPassword })
+
+    await newUser.save()
+
+    return res.json({ username, password: hashedPassword })
+  } catch (error) {
+    res.status(500).send(error)
+  }
 }
 
 export async function logout (req, res) {
-    res.clearCookie('access_token').json({ message: 'Logout successfully.'})
+  res.clearCookie('access_token').json({ message: 'Logout successfully.' })
 }
 
 export async function getUser (req, res) {
-    //TODO get user's data who send the request
+  const { userId } = req.params
+
+  try {
+    const user = User.findById(userId)
+
+    if (user) {
+      return res.status(200).json({ user })
+    }
+
+    return res.status(404).json({ message: 'User not found.' })
+  } catch (error) {
+    return res.status(500).json({ error })
+  }
 }
 
 export async function getAllUsers (req, res) {
-    //TODO get all users in db
+  try {
+    const users = User.find()
+    console.log(users)
+
+    return res.status(200).json({ users })
+  } catch (error) {
+    return res.status(500).json({ error })
+  }
 }
