5621f430015850a14631306993a688a571a37dd2
Mariano Lotero Rolon <marianonehuenlr@gmail.com>
2024-07-17 17:54:22 -0300

    routes order

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,14 +1,12 @@
 import express from 'express'
-import {login, register, logout, getUser, getAllUsers} from '../controllers/userController.js'
+import { login, register, logout, getUser, getAllUsers } from '../controllers/userController.js'
 
 const userRouter = express.Router()
 
-userRouter.get('/', getAllUsers)
-userRouter.get('/:id', getUser)
 userRouter.post('/register', register)
 userRouter.post('/login', login)
 userRouter.post('/logout', logout)
-
-
+userRouter.get('/:id', getUser)
+userRouter.get('/', getAllUsers)
 
 export default userRouter
