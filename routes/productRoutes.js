3d34433669e7e5fe86a9973d624375cb15883a9d
Mariano Lotero Rolon <marianonehuenlr@gmail.com>
2024-07-24 21:25:20 -0300

    auth user middleware

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,12 +1,13 @@
 import express from 'express'
 import { add, deleteProduct, getAll, getById, update } from '../controllers/productController.js'
+import { authUser } from '../middlewares/authUser.js'
 
 const productRouter = express.Router()
 
-productRouter.post('/', add)
-productRouter.delete('/:id', deleteProduct)
+productRouter.post('/', authUser, add)
+productRouter.delete('/:id', authUser, deleteProduct)
 productRouter.get('/', getAll)
 productRouter.get('/:id', getById)
-productRouter.patch('/:id', update)
+productRouter.patch('/:id', authUser, update)
 
 export default productRouter
