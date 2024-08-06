e13368ea7645d85ba08130843f52bf5632e9ebe5
Mariano Lotero Rolon <marianonehuenlr@gmail.com>
2024-08-06 17:58:28 -0300

    + get me route

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,11 +1,13 @@
 import express from 'express'
-import { login, register, logout, getUser, getAllUsers } from '../controllers/userController.js'
+import { login, register, logout, getUser, getAllUsers, getMe } from '../controllers/userController.js'
+import { authUser } from '../middlewares/authUser.js'
 
 const userRouter = express.Router()
 
 userRouter.post('/register', register)
 userRouter.post('/login', login)
 userRouter.post('/logout', logout)
+userRouter.get('/me', authUser, getMe)
 userRouter.get('/:id', getUser)
 userRouter.get('/', getAllUsers)
 
