daff9db78bbbe538b46583c77d312fbfe5c1a8e2
Mariano Lotero Rolon <marianonehuenlr@gmail.com>
2024-08-07 01:54:32 -0300

    multer handler

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,13 +1,45 @@
 import express from 'express'
 import { add, deleteProduct, getAll, getById, update } from '../controllers/productController.js'
 import { authUser } from '../middlewares/authUser.js'
+import { dirname, join, extname } from 'path'
+import { fileURLToPath } from 'url'
+import multer from 'multer'
+
+const CURRENT_DIR = dirname(fileURLToPath(import.meta.url))
+const MIMETYPES = ['image/jpeg', 'image/png']
+
+const multerUpload = multer({
+  storage: multer.diskStorage({
+    destination: join(CURRENT_DIR, '../uploads'),
+    filename: (req, file, cb) => {
+      const fileExtension = extname(file.originalname)
+      const fileName = file.originalname.split(fileExtension)[0]
+      cb(null, `${fileName}-${Date.now()}${fileExtension}`)
+    }
+  }),
+  fileFilter: (req, file, cb) => {
+    if (MIMETYPES.includes(file.mimetype)) cb(null, true)
+    else cb(new Error(`Image must be one of the following types ${MIMETYPES.join(' ')}`))
+  },
+  limits: {
+    fieldSize: 10000000
+  }
+})
 
 const productRouter = express.Router()
 
-productRouter.post('/', authUser, add)
+productRouter.post('/', (req, res, next) => {
+  multerUpload.single('image')(req, res, (err) => {
+    if (err) {
+      return res.status(400).json({ error: err.message })
+    }
+
+    add(req, res)
+  })
+})
 productRouter.delete('/:id', authUser, deleteProduct)
 productRouter.get('/', getAll)
-productRouter.get('/:id', getById)
+productRouter.get('/:productId', getById)
 productRouter.patch('/:id', authUser, update)
 
 export default productRouter
