7e80f2dec48887483a203ffbed57d13213a69095
Mariano Lotero Rolon <marianonehuenlr@gmail.com>
2024-08-06 23:53:55 -0300

    + category model

---
+++ models/Category.js
@@ -0,0 +1,19 @@
+import { Schema, model } from 'mongoose'
+
+const categorySchema = new Schema({
+  name: {
+    type: String
+  }
+})
+
+categorySchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+const Category = model('categories', categorySchema)
+
+export default Category
