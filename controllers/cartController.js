d2c11348bfb72be587c128f0a0b4f17990f4abd8
Mariano Lotero Rolon <marianonehuenlr@gmail.com>
2024-08-07 17:27:54 -0300

    add product to cart

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -1,29 +1,37 @@
 import Cart from '../models/Cart.js'
 import 'dotenv/config'
+import Product from '../models/Product.js'
 
 export async function add (req, res) {
   // Create cart if it's first item
   const user = req.session.user
-  const { productId, quantity } = req.body
-  let userCart = await Cart.find({ user: user.userId })
+  const { productId, quantity } = req.body.data
+  let [userCart] = await Cart.find({ user: user.userId })
 
   if (!userCart) {
     const newCart = new Cart({ user: user.userId, items: [], totalPrice: 0 })
     userCart = await newCart.save()
+    console.log('Crea un carrito nuevo')
   }
-
+  console.log(userCart)
   // Control if item already exists
   const itemExists = userCart.items.findIndex(item => item.product.toString() === productId)
 
   // If index is -1 item does not exist in cart
   if (itemExists === -1) {
-    userCart.items.push({ product: productId, quantity })
+    userCart.items.push({ product: productId, quantity: 1 })
   } else {
     // Change for new quantity
     userCart.items[itemExists].quantity = quantity
   }
+  let newTotalPrice = 0
+  for (let i = 0; i < userCart.items.length; i++) {
+    const item = await Product.findById(userCart.items[i].product)
+    newTotalPrice += item.price
+  }
+
+  userCart.totalPrice = newTotalPrice
   await userCart.save()
-  // TODO UPDATE TOTAL PRICE
 
   return res.status(201).json({ userCart })
 }
