cc24c82becf43336a718169d7cdc633bd03249d4
Mariano Lotero Rolon <marianonehuenlr@gmail.com>
2024-08-14 11:57:23 -0300

    update cart route

--- routes/cartRoutes.js
+++ routes/cartRoutes.js
@@ -1,10 +1,11 @@
 import express from 'express'
-import { add, getCart } from '../controllers/cartController.js'
+import { add, getCart, deleteItem } from '../controllers/cartController.js'
 import { authUser } from '../middlewares/authUser.js'
 
 const cartRouter = express.Router()
 
 cartRouter.post('/', authUser, add)
 cartRouter.get('/:userId', authUser, getCart)
+cartRouter.patch('/item/:product', authUser, deleteItem)
 
 export default cartRouter
