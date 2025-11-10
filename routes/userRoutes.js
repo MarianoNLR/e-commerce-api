c9aa183618304aaeca60defdde7ced970729a146
MarianoNLR <marianonehuenlr@gmail.com>
2025-11-10 20:36:58 -0300

    + routes for google oauth

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,11 +1,14 @@
 import express from 'express'
-import { login, register, logout, getUser, getAllUsers, getMe } from '../controllers/userController.js'
+import { login, loginUserFromGoogle, register, logout, getUser, getAllUsers, getMe } from '../controllers/userController.js'
 import { authUser } from '../middlewares/authUser.js'
+import passport from 'passport'
 
 const userRouter = express.Router()
 
 userRouter.post('/register', register)
 userRouter.post('/login', login)
+userRouter.get('/google', passport.authenticate('google', { scope: ['profile', 'email'] }))
+userRouter.get('/google/callback', passport.authenticate('google', { session: false }), loginUserFromGoogle)
 userRouter.post('/logout', logout)
 userRouter.get('/me', authUser, getMe)
 userRouter.get('/:id', getUser)
