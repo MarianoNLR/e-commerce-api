aa2dd3096133c7a7c21084400aaa3e839d460962
MarianoNLR <marianonehuenlr@gmail.com>
2025-11-15 21:47:25 -0300

    fix

--- middlewares/authUser.js
+++ middlewares/authUser.js
@@ -4,21 +4,31 @@ import User from '../models/User.js'
 const { JWT_SECRET } = process.env
 
 export const authUser = async (req, res, next) => {
+  console.log(req.get('authorization'))
   const authorization = req.get('authorization')
   let token = ''
   if (authorization && authorization.toLowerCase().startsWith('bearer')) {
+    console.log('Authenticating user...')
     token = authorization.substring(7)
-  }
-  const decodedToken = jwt.verify(token, JWT_SECRET)
-
-  if (!token || !decodedToken.userId) {
+  } else {
     return res.status(401).json({ error: 'token missing or invalid' })
   }
+  try {
+    const decodedToken = jwt.verify(token, JWT_SECRET)
+    console.log('Decoded token:', decodedToken)
 
-  const { userId } = decodedToken
-  req.userId = userId
-  req.user = await User.findById(userId).select('-password')
+    if (!token || !decodedToken.userId) {
+      return res.status(401).json({ error: 'token missing or invalid' })
+    }
+
+    const { userId } = decodedToken
+    req.userId = userId
+    req.user = await User.findById(userId).select('-password')
   next()
+  } catch (error) {
+    return res.status(401).json({ error: 'token missing or invalid' })
+  }
+  
 
   // To use with cookies
   // if (!req.cookies || !req.cookies.access_token) {
