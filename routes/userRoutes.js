1e394a0e81386cb302f468124a4b8f67045d3fdf
MarianoNLR <marianonehuenlr@gmail.com>
2025-11-15 21:52:15 -0300

    + endopint to check emaila availability. + endpoint to save complete data from google signup

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,17 +1,40 @@
 import express from 'express'
-import { login, loginUserFromGoogle, register, logout, getUser, getAllUsers, getMe } from '../controllers/userController.js'
+import { login, loginUserFromGoogle, register, completeGoogleSignup, logout, getUser, getAllUsers, getMe } from '../controllers/userController.js'
 import { authUser } from '../middlewares/authUser.js'
+import User from '../models/User.js'
 import passport from 'passport'
 
 const userRouter = express.Router()
 
 userRouter.post('/register', register)
+userRouter.post('/complete-google-signup', completeGoogleSignup)
 userRouter.post('/login', login)
-userRouter.get('/google', passport.authenticate('google', { scope: ['profile', 'email'] }))
+userRouter.get('/google', 
+    (req, res, next) => {
+        passport.authenticate('google', {
+            scope: ['profile', 'email'], 
+            state: req.query.state || "default" 
+        })(req, res, next)
+    }
+)
 userRouter.get('/google/callback', passport.authenticate('google', { session: false }), loginUserFromGoogle)
 userRouter.post('/logout', logout)
 userRouter.get('/me', authUser, getMe)
 userRouter.get('/:id', getUser)
+userRouter.post('/email-check', async (req, res) => {
+    console.log(req.body)
+    const { email } = req.body
+    if (!email) {
+        return res.status(400).json({ message: 'Email is required' })
+    }
+    // Check if the email exists in the database
+    const user = await User.findOne({ email })
+    if (user) {
+        return res.status(200).json({ email: user.email })
+    }
+    return res.status(200).json({ email: null })
+})
 userRouter.get('/', getAllUsers)
+
 
 export default userRouter
