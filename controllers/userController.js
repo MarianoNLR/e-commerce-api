bb5beca5b93345bd410d846f3e195458aab000af
MarianoNLR <marianonehuenlr@gmail.com>
2025-11-21 19:55:17 -0300

    google auth handler and using email instead of username

--- controllers/userController.js
+++ controllers/userController.js
@@ -6,21 +6,20 @@ import 'dotenv/config'
 const { JWT_SECRET } = process.env
 
 export async function login (req, res) {
-  const { username, password } = req.body
+  const { email, password } = req.body
 
   try {
-    const user = await User.findOne({ username })
+    const user = await User.findOne({ email })
 
     if (!user) {
-      return res.status(401).json({ message: 'Username or password incorrect.' })
+      return res.status(401).json({ message: 'Email or password incorrect.' })
     }
 
     const passwordMatch = await bcrypt.compare(password, user.password)
 
     if (!passwordMatch) {
-      return res.status(401).json({ message: 'Username or password incorrect.' })
+      return res.status(401).json({ message: 'Email or password incorrect.' })
     }
-
     const token = jwt.sign({ userId: user._id}, JWT_SECRET)
 
     // res.cookie('access_token', token, {
@@ -35,35 +34,91 @@ export async function login (req, res) {
   }
 }
 
-export const loginUserFromGoogle = async (req, res) => {
-  console.log('Google login callback triggered', req.user)
-  if (req.user) {
-    const token = jwt.sign({ userId: req.user._id, role: req.user.role }, JWT_SECRET, { expiresIn: '7d' })
-    req.token = token
-    console.log('Generated JWT token for Google user:', token)
-    res.redirect(`http://localhost:5173/auth/google/success/?token=${token}`)
-    //return res.status(200).json({ user: req.user, token })
-    return
+export const completeGoogleSignup = async (req, res) => {
+  
+  try {
+    const { token, name, lastName } = req.body
+    const decoded = jwt.verify(token, JWT_SECRET)
+    const { googleId, email } = decoded
+    const existingUser = await User.findOne({ email })
+    if (existingUser) {
+      return res.status(400).json({ message: 'Email already in use.' })
+    }
+
+    const newUser = new User({
+      googleId,
+      name,
+      lastName,
+      email,
+    })
+    console.log(newUser)
+    await newUser.save()
+
+    const authToken = jwt.sign({ userId: newUser._id, role: newUser.role }, JWT_SECRET, { expiresIn: '7d' })
+    return res.status(201).json({ 
+      token: authToken, 
+      user: { name: newUser.name, lastName: newUser.lastName, email: newUser.email }
+    })
+  } catch (error) {
+    return res.status(500).json({ error: 'Failed to complete Google signup.' })
   }
-  return res.status(401).json({ message: 'Not authenticated' })
 }
 
+export const loginUserFromGoogle = async (req, res) => {
+  if (!req.user) {
+    return res.status(401).json({ message: 'Not authenticated' })
+  }
+
+  if (req.user.isNewUser) {
+    // Handle new user registration flow
+    const { googleId, email, isNewUser } = req.user
+    const token = jwt.sign({ googleId, email }, JWT_SECRET, { expiresIn: '10m' })
+
+    const html = `
+      <script>
+        window.opener.postMessage(
+          ${JSON.stringify({ token, email, isNewUser })},
+        'http://localhost:5173'
+        );
+        window.close();
+      </script>
+    `
+    return res.send(html)
+  }
+  const { googleId, email } = req.user
+
+  const token = jwt.sign({ userId: req.user._id, role: req.user.role }, JWT_SECRET, { expiresIn: '7d' })
+
+  const html = `
+    <script>
+      window.opener.postMessage(
+        ${JSON.stringify({ token, email })},
+      'http://localhost:5173'
+      );
+      window.close();
+    </script>
+  `
+    return res.send(html)
+
+  }
+
 export async function register (req, res) {
-  const { username, password, confirmPassword } = req.body.data
+  const { name, lastName, email, password, confirmPassword } = req.body
 
   if (password !== confirmPassword) return res.status(400).json({ message: 'Passwords must match.' })
 
   try {
-    const userExists = await User.findOne({ username })
+    const userExists = await User.findOne({ email })
 
-    if (userExists) return res.status(400).json({ message: 'Username already exists.' })
+    if (userExists) return res.status(400).json({ message: 'Email already exists.' })
 
     const hashedPassword = await bcrypt.hash(password, 10)
-    const newUser = new User({ username, password: hashedPassword })
+    const newUser = new User({ name, lastName, email, password: hashedPassword })
 
-    await newUser.save()
-
-    return res.json({ username, password: hashedPassword })
+    const result = await newUser.save()
+    
+    return res.status(201).json({ email, name, lastName })
+    
   } catch (error) {
     return res.status(500).json({ error })
   }
