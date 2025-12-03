ac108ae8d3afa97bd1ec4f098ccf8a8e0d2bfa91
MarianoNLR <marianonehuenlr@gmail.com>
2025-12-03 15:36:51 -0300

    refactor: separate controllers and services

--- controllers/productController.js
+++ controllers/productController.js
@@ -2,86 +2,64 @@ import Product from '../models/Product.js'
 import 'dotenv/config'
 import path, { extname } from 'path'
 import { unlink } from 'fs/promises';
+import * as productService from '../services/productService.js'
 
 export async function getAll (req, res) {
-  const { categoryId } = req.params
-  const filters = {}
-
-  if (categoryId) {
-    filters.categoryId = categoryId
-  }
-
   try {
-    const products = await Product.find(filters).collation({ locale: 'es', strength: 2 }).sort({ price: -1 })
-      .populate('categoryId')
+    const { categoryId } = req.params
+    const filters = {}
+    if (categoryId) {
+      filters.categoryId = categoryId
+    }
+    const products = await productService.getAll({categoryId, filters})
 
     return res.status(200).json({ products })
   } catch (error) {
-    console.log(error)
-    return res.status(500).json({ error })
+    console.error("Error fetching products:", error)
+    return res.status(error.status || 500).json({ error: error.message || 'Internal server error.' })
   }
 }
 
 export async function getBySearch (req, res) {
-  const { q } = req.query
-  const filters = {}
-  console.log(q)
-  if (q) {
-    filters.name = { $regex: q, $options: 'i' }
-  }
+
 
   try {
-    const products = await Product.find(filters)
-      .populate('categoryId')
+
+    const { q } = req.query
+    const filters = {}
+    if (q) {
+      filters.name = { $regex: q, $options: 'i' }
+    }
+    const products = await productService.getAll({ filters })
 
     return res.status(200).json({ products })
   } catch (error) {
-    console.log(error)
-    return res.status(500).json({ error })
+    console.error("Error fetching products by search:", error)
+    return res.status(error.status || 500).json({ error: error.message || 'Internal server error.' })
   }
 }
 
 export async function getById (req, res) {
-  const { productId } = req.params
-
   try {
-    const product = await Product.findById(productId)
-
-    if (!product) return res.status(404).json({ message: 'Product not found.' })
+    const { productId } = req.params
+    const product = await productService.getById({ productId })
 
     return res.status(200).json({ product })
   } catch (error) {
-    return res.status(500).json({ error })
+    return res.status(error.status || 500).json({ error: error.message || 'Internal server error.' })
   }
 }
 
 export async function add (req, res) {
-  const { name, price, quantity, categoryId, description, deletedImages } = req.body
-  const { files : images } = req
-  const imagesURLs = []
-  
-  images.filter().forEach(image => {
-    const fileExtension = extname(image.originalname)
-    const fileName = image.filename.split(fileExtension)[0]
-    const fileFullName = `${fileName}${fileExtension}`
-    imagesURLs.push(fileFullName)
-  });
-  
   try {
-    const newProduct = new Product(
-      {
-        name,
-        price,
-        quantity,
-        categoryId,
-        description,
-        imagesURLs: imagesURLs
-      })
-    await newProduct.save()
+    const { name, price, quantity, categoryId, description } = req.body
+    const { files : images } = req
+
+    await productService.add({ name, price, quantity, categoryId, description, images })
 
     return res.status(201).json({ newProduct })
   } catch (error) {
-    return res.status(500).json({ error })
+    return res.status(error.status || 500).json({ error: error.message || 'Internal server error.' })
   }
 }
 
@@ -94,47 +72,48 @@ export async function update (req, res) {
   
   //console.log(req)
   try {
-    // Update product simple fields first before handling images
-    await Product.findByIdAndUpdate(id, productsUpdate)
+    // // Update product simple fields first before handling images
+    // await Product.findByIdAndUpdate(id, productsUpdate)
 
-    // Get current images from product
-    const currentImages = await Product.findById(id).select('imagesURLs -_id')
+    // // Get current images from product
+    // const currentImages = await Product.findById(id).select('imagesURLs -_id')
 
-    // Determine which images to delete by filtering out the ones to keep
-    const toDelete = []
-    if (imagesToKeep.length === 0) {
-        toDelete.push(...currentImages.imagesURLs)
-    } else {
-      toDelete = currentImages.imagesURLs.filter(img => !imagesToKeep.includes(img))
-    }
+    // // Determine which images to delete by filtering out the ones to keep
+    // const toDelete = []
+    // if (imagesToKeep.length === 0) {
+    //     toDelete.push(...currentImages.imagesURLs)
+    // } else {
+    //   toDelete = currentImages.imagesURLs.filter(img => !imagesToKeep.includes(img))
+    // }
     
 
-    //Delete images that are not in imagesToKeep from uploads folder and from product document
-    for (const image of toDelete) { 
-      try {
-        const imgPath = path.join(process.cwd(), 'uploads', image)
-        await unlink(imgPath)
-      } catch (error) {
-        if (error.code !== 'ENOENT') {
-          console.error(`File ${image} not found, skipping deletion.`)
-        }
-      }
-      await Product.updateOne({ _id: id }, { $pull: { imagesURLs: image } })
-    }
+    // //Delete images that are not in imagesToKeep from uploads folder and from product document
+    // for (const image of toDelete) { 
+    //   try {
+    //     const imgPath = path.join(process.cwd(), 'uploads', image)
+    //     await unlink(imgPath)
+    //   } catch (error) {
+    //     if (error.code !== 'ENOENT') {
+    //       console.error(`File ${image} not found, skipping deletion.`)
+    //     }
+    //   }
+    //   await Product.updateOne({ _id: id }, { $pull: { imagesURLs: image } })
+    // }
 
-    // Add new images to product document
-    const newImagesURLs = [];
-    if (newImages && newImages.length > 0) {
-      for (const image of newImages) {
-        newImagesURLs.push(image.filename);
-      }
-      await Product.updateOne({ _id: id }, { $push: { imagesURLs: { $each: newImagesURLs } } });
-    }
-    const updatedProduct = await Product.findById(id)
-    res.status(200).json({ result: updatedProduct })
+    // // Add new images to product document
+    // const newImagesURLs = [];
+    // if (newImages && newImages.length > 0) {
+    //   for (const image of newImages) {
+    //     newImagesURLs.push(image.filename);
+    //   }
+    //   await Product.updateOne({ _id: id }, { $push: { imagesURLs: { $each: newImagesURLs } } });
+    // }
+    // const updatedProduct = await Product.findById(id)
+    // res.status(200).json({ result: updatedProduct })
+    const result = await productService.update({ productId: id, productsUpdate, newImages, imagesToKeep })
   } catch (error) {
-    console.log(error)
-    return res.status(500).json({ error })
+    console.error('Error updating product:', error)
+    return res.status(error.status || 500).json({ error: error.message || 'Internal server error.' })
   }
 }
 
@@ -142,45 +121,40 @@ export async function updateProductStock (req, res) {
   console.log(req)
   const { productId } = req.params
   const { stock } = req.body
-  console.log(stock)
 
   try {
-    const updatedProduct = await Product.findByIdAndUpdate(productId, { quantity: stock }, { new: true })
+    const result = await productService.setStock(productId, stock)
 
-    if (!updatedProduct) return res.status(404).json({ message: 'Product not found.' })
-
-    return res.status(200).json({ result: updatedProduct })
+    return res.status(200).json({ result })
   } catch (error) {
-    return res.status(500).json({ error })
+    console.error('Error updating product stock:', error)
+    return res.status(error.status || 500).json({ error: error.message || 'Internal server error.' })
   }
 }
 
 export async function deleteProduct (req, res) {
   const { id: productId } = req.params
-  console.log(productId)
-  const productExists = await Product.findById(productId)
-
-  if (!productExists) return res.status(404).json({ message: 'Product not found.' })
 
   try {
-    await Product.deleteOne({ _id: productId })
+    await productService.deleteProduct({ productId })
     return res.status(200).json({ message: 'Product has been removed.' })
   } catch (error) {
-    return res.status(500).json({ error })
+    console.error('Error deleting product:', error)
+    return res.status(error.status || 500).json({ error: error.message || 'Internal server error.' })
   }
 }
 
-export async function updateProductStockPurchase (productId, quantity) {
-  try {
-    const product = await Product.findById(productId)
-    if (product) {
-      product.quantity -= quantity
-      await product.save()
-      return true
-    }
-    return false
-  } catch (error) {
-    console.error('It seems there was an error with product stock:', error)
-    return false
-  }
-}
+// export async function updateProductStockPurchase (req, res) {
+//   try {
+//     const product = await Product.findById(productId)
+//     if (product) {
+//       product.quantity -= quantity
+//       await product.save()
+//       return true
+//     }
+//     return false
+//   } catch (error) {
+//     console.error('It seems there was an error with product stock:', error)
+//     return false
+//   }
+// }
---
+++ services/productService.js
@@ -0,0 +1,137 @@
+import Product from "../models/Product.js"
+import path, { extname } from "path";
+import { unlink } from "fs/promises";
+import 'dotenv/config'
+
+export async function getAll({categoryId, filters}) {
+    if (categoryId) {
+      filters.categoryId = categoryId
+    }
+
+    const products = await Product.find(filters).collation({ locale: 'es', strength: 2 }).sort({ price: -1 })
+        .populate('categoryId')
+    
+    if (!products) {
+        const err = new Error('No products found')
+        err.status = 404
+        throw err
+    }
+    return products
+}
+
+export async function getBySearch({q, filters}) {
+
+    if (q) {
+      filters.name = { $regex: q, $options: 'i' }
+    }
+
+    const products = await Product.find(filters)
+        .populate('categoryId')
+    
+    if (!products) {
+        const err = new Error('No products found')
+        err.status = 404
+        throw err
+    }
+    return products
+}
+
+export async function getById({ productId }) {
+    const product = await Product.findById(productId)
+
+    if (!product) {
+        const err = new Error('Product not found')
+        err.status = 404
+        throw err
+    }
+    return product
+}
+
+export async function add({ name, price, quantity, categoryId, description, images }) {
+    const imagesURLs = []
+
+    images.filter().forEach(image => {
+    const fileExtension = extname(image.originalname)
+    const fileName = image.filename.split(fileExtension)[0]
+    const fileFullName = `${fileName}${fileExtension}`
+    imagesURLs.push(fileFullName)
+    });
+
+    const newProduct = new Product({
+        name,
+        price,
+        quantity,
+        categoryId,
+        description,
+        imagesURLs: imagesURLs
+    })
+
+    const result = await newProduct.save()
+    return result
+}
+
+export async function update({ productId, productsUpdate, newImages, imagesToKeep }) {
+    // Update product simple fields first before handling images
+    await Product.findByIdAndUpdate(productId, productsUpdate, { new: true })
+
+    // Get current images from product
+    const currentImages = await Product.findById(productId).select('imagesURLs -_id')
+
+     // Determine which images to delete by filtering out the ones to keep
+    const imagesToDelete = []
+    if (imagesToKeep.length === 0) {
+        imagesToDelete.push(...currentImages.imagesURLs)
+    } else {
+        imagesToDelete.push(...currentImages.imagesURLs.filter(img => !imagesToKeep.includes(img)))
+    }
+
+    //Delete images that are not in imagesToKeep from uploads folder and from product document
+    for (const image of imagesToDelete) {
+        try {
+            const imagePath = path.join(process.cwd(), process.env.UPLOADS_FOLDER, image)
+            await unlink(imagePath)
+        } catch (error) {
+            if (error.code !== 'ENOENT') {
+                console.error(`File ${image} not found.`, error)
+            }
+        }
+        await Product.updateOne({ _id: productId }, { $pull: { imagesURLs: image } })
+    }
+
+    // Add new images to product document
+    const newImagesURLs = []
+    if (newImages && newImages.length > 0) {
+        for (const image of newImages) {
+            newImagesURLs.push(image.filename)
+        }
+        await Product.updateOne({ _id: productId }, { $push: { imagesURLs: { $each: newImagesURLs } } })
+    }
+
+    const updatedProduct = await Product.findById(productId)
+    return updatedProduct
+}
+
+export async function deleteProduct({ productId }) {
+    const result = await Product.deleteOne({ _id: productId })
+
+    if (result.deletedCount === 0) {
+        const err = new Error('Product not found')
+        err.status = 404
+        throw err
+    }
+
+    return { message: 'Product deleted successfully' }
+}
+
+export async function updateProductStock({ productId, update}) {
+    return await Product.findByIdAndUpdate(productId, update, { new: true })
+
+}
+
+export async function decreaseStock(productId, amount) {
+    return updateProductStock({ productId, update: { $inc: { quantity: -amount } } })
+}
+
+export async function setStock(productId, newStock) {
+    return updateProductStock({ productId, update: { quantity: newStock } })
+}
