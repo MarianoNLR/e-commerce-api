36d591f2618e1a9f1f01ed83dce44930a07fdbbd
MarianoNLR <marianonehuenlr@gmail.com>
2025-12-04 22:49:00 -0300

    + get, put and delete routes

--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -1,9 +1,12 @@
 import express from 'express'
-import { getAll, addCategory } from '../controllers/categoryController.js'
+import { getAll, addCategory, getCategoryById ,deleteCategory, updateCategory } from '../controllers/categoryController.js'
 
 const categoryRouter = express.Router()
 
 categoryRouter.get('/', getAll)
 categoryRouter.post('/', addCategory)
+categoryRouter.get('/:categoryId', getCategoryById)
+categoryRouter.delete('/:categoryId', deleteCategory)
+categoryRouter.put('/:categoryId', updateCategory)
 
 export default categoryRouter
