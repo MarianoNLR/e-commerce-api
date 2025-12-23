a229319bfe16a521b95283b6a3cd37996cb6b8b8
MarianoNLR <marianonehuenlr@gmail.com>
2025-12-22 21:58:41 -0300

    refactor

--- middlewares/authUser.js
+++ middlewares/authUser.js
@@ -1,34 +1,39 @@
 import jwt, { decode } from 'jsonwebtoken'
 import { parse } from 'cookie'
 import User from '../models/User.js'
+import 'dotenv/config'
+import { verifyToken } from '../utils/verifyToken.js'
+import { UnauthorizedError } from '../errors/UnauthorizedError.js'
 const { JWT_SECRET } = process.env
 
 export const authUser = async (req, res, next) => {
-  console.log(req.get('authorization'))
+
+  // Get token from Authorization header
   const authorization = req.get('authorization')
-  let token = ''
-  if (authorization && authorization.toLowerCase().startsWith('bearer')) {
-    console.log('Authenticating user...')
-    token = authorization.substring(7)
-  } else {
-    return res.status(401).json({ error: 'token missing or invalid' })
+
+  // if authorization header is missing or doesn't start with 'Bearer ' throw error
+  if (!authorization?.startsWith('Bearer ')) {
+    return next(new UnauthorizedError('Token missing or invalid.'))
   }
-  try {
-    const decodedToken = jwt.verify(token, JWT_SECRET)
-    console.log('Decoded token:', decodedToken)
 
-    if (!token || !decodedToken.userId) {
-      return res.status(401).json({ error: 'token missing or invalid' })
-    }
+  // Get only the token part
+  const token = authorization.substring(7)
 
-    const { userId } = decodedToken
-    req.userId = userId
-    req.user = await User.findById(userId).select('-password')
-  next()
-  } catch (error) {
-    return res.status(401).json({ error: 'token missing or invalid' })
+  // Verify token and get decoded data
+  const decodedToken = verifyToken(token)
+
+  if (!decodedToken?.userId) {
+    return next(new UnauthorizedError('Token missing or invalid.'))
   }
   
+  // Set user info in request object
+  const user = await User.findById(decodedToken.userId).select('-password')
+  if (!user) {
+    return next(new UnauthorizedError('User not found for token.'))
+  }
+  req.user = user
+  req.userId = user._id
+  next()
 
   // To use with cookies
   // if (!req.cookies || !req.cookies.access_token) {
