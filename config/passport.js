b332d1f570d69bbe386fc42ab31403ecf81bfb80
MarianoNLR <marianonehuenlr@gmail.com>
2026-01-20 21:41:11 -0300

    update: base url for callback from .env

--- config/passport.js
+++ config/passport.js
@@ -10,7 +10,7 @@ passport.use(
         {
             clientID: process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-            callbackURL: `${process.env.BASE_URL}/users/google/callback`
+            callbackURL: `${process.env.BASE_URL}/auth/google/callback`
         },
         async (accessToken, refreshToken, profile, done) => {
             return done(null, {
