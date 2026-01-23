46aff6d0705276a298f36a214993541cd3a20c63
MarianoNLR <marianonehuenlr@gmail.com>
2026-01-22 21:39:14 -0300

    fix: adde return to response

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -7,14 +7,14 @@ export function errorHandler(err, req, res, next) {
     }
 
     if (err.isOperational) {
-        res.status(err.statusCode).json({
+        return res.status(err.statusCode).json({
             status: 'error',
             message: err.message
         });
     }
 
     console.error('Unexpected Error:', err);
-    res.status(500).json({
+    return res.status(500).json({
         status: 'error',
         message: 'Internal Server Error'
     });
