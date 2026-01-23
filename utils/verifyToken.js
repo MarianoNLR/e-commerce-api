9724d835c9f56b8b5a3af572853c1ef5760cbd67
MarianoNLR <marianonehuenlr@gmail.com>
2026-01-22 21:40:31 -0300

    + try catch to throw custom error

--- utils/verifyToken.js
+++ utils/verifyToken.js
@@ -2,9 +2,23 @@ import jwt from 'jsonwebtoken'
 import dotenv from 'dotenv/config'
 import { UnauthorizedError } from '../errors/UnauthorizedError.js'
 
-const { JWT_SECRET } = process.env
+const { JWT_SECRET, JWT_REFRESH_TOKEN_SECRET } = process.env
 
 // Used to verify JWT tokens, if error occurs, it will be handled in errorHandler middleware
 export function verifyToken(token) {
-    return jwt.verify(token, JWT_SECRET)
+    try {
+        return jwt.verify(token, JWT_SECRET)
+    } catch (error) {
+        throw new UnauthorizedError('Invalid token.')
+    }
+    
+}
+
+export function verifyRefreshToken(token) {
+    try {
+        return jwt.verify(token, process.env.JWT_REFRESH_TOKEN_SECRET)    
+    } catch (error) {
+        throw new UnauthorizedError('Invalid refresh token.')
+    }
+    
 }
