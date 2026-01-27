7507861f21c826ef28bc1d10d27c7774e5f1cc70
MarianoNLR <marianonehuenlr@gmail.com>
2026-01-26 22:19:11 -0300

    add validation middleware for category routes

--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -1,12 +1,14 @@
 import express from 'express'
 import { getAll, addCategory, getCategoryById ,deleteCategory, updateCategory } from '../controllers/categoryController.js'
+import { validate } from '../middlewares/validate.js'
+import { getByIdSchema, addCategorySchema, deleteCategorySchema, updateCategorySchema} from '../schemas/categorySchema.js'
 
 const categoryRouter = express.Router()
 
 categoryRouter.get('/', getAll)
-categoryRouter.post('/', addCategory)
-categoryRouter.get('/:categoryId', getCategoryById)
-categoryRouter.delete('/:categoryId', deleteCategory)
-categoryRouter.put('/:categoryId', updateCategory)
+categoryRouter.post('/', validate(addCategorySchema), addCategory)
+categoryRouter.get('/:categoryId', validate(getByIdSchema), getCategoryById)
+categoryRouter.delete('/:categoryId', validate(deleteCategorySchema), deleteCategory)
+categoryRouter.put('/:categoryId', validate(updateCategorySchema), updateCategory)
 
 export default categoryRouter
