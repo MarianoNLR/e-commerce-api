bd5452db657a227cacef9e4e6a22654d4468451c
MarianoNLR <marianonehuenlr@gmail.com>
2026-02-04 22:11:11 -0300

    refactor verifyToken and verifyRefreshToken functions to remove error handling in order to get jwt error info

--- utils/verifyToken.js
+++ utils/verifyToken.js
@@ -6,19 +6,10 @@ const { JWT_SECRET, JWT_REFRESH_TOKEN_SECRET } = process.env
 
 // Used to verify JWT tokens, if error occurs, it will be handled in errorHandler middleware
 export function verifyToken(token) {
-    try {
-        return jwt.verify(token, JWT_SECRET)
-    } catch (error) {
-        throw new UnauthorizedError('Invalid token.')
-    }
+    return jwt.verify(token, JWT_SECRET)
     
 }
 
 export function verifyRefreshToken(token) {
-    try {
-        return jwt.verify(token, process.env.JWT_REFRESH_TOKEN_SECRET)    
-    } catch (error) {
-        throw new UnauthorizedError('Invalid refresh token.')
-    }
-    
+    return jwt.verify(token, JWT_REFRESH_TOKEN_SECRET)
 }
