4b8a84fe2c860f55bd26ff1183b6c8271a906906
MarianoNLR <marianonehuenlr@gmail.com>
2026-02-04 22:10:19 -0300

    refactor cartRoutes to add validation for addCart schema and remove unused getById schema

--- routes/cartRoutes.js
+++ routes/cartRoutes.js
@@ -1,13 +1,13 @@
 import express from 'express'
 import { add, getCart, deleteItem } from '../controllers/cartController.js'
 import { authUser } from '../middlewares/authUser.js'
-import { getByIdSchema, deleteCartItemSchema } from '../validators/cart.schema.js'
+import { addCartSchema, deleteCartItemSchema } from '../validators/cart.schema.js'
 import { validate } from '../middlewares/validate.js'
 
 const cartRouter = express.Router()
 
-cartRouter.post('/', authUser, add)
-cartRouter.get('/:userId', authUser, validate(getByIdSchema), getCart)
+cartRouter.post('/', validate(addCartSchema), authUser, add)
+cartRouter.get('/:userId', authUser, getCart)
 cartRouter.patch('/item/:product', authUser, validate(deleteCartItemSchema), deleteItem)
 
 export default cartRouter
