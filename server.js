fdb362c49bea3b44e17934dc946ad6aff1fdaf46
MarianoNLR <marianonehuenlr@gmail.com>
2026-02-13 18:19:05 -0300

    fix: remove MongoDB URI variable and update connection method

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ import app from './index.js'
 import { connectDB } from './mongo.js'
 
 const PORT = process.env.PORT ?? 3000
-const MONGODB_URI = process.env.MONGODB_URI ?? 'mongodb://localhost:27017/ecommerce'
-await connectDB(MONGODB_URI)
+// const MONGODB_URI = process.env.MONGODB_URI ?? 'mongodb://localhost:27017/ecommerce'
+
+// Connect to MongoDB using the URI from environment variables
+await connectDB()
 
 app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server listening on http://localhost:${PORT}`)
