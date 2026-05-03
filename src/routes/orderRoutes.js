71c6cab7cc63c09848781757288a5eef4cfcc544
MarianoNLR <marianonehuenlr@gmail.com>
2026-05-02 23:15:22 -0300

    feat: add payment proof upload functionality and update order status to pending_validation

--- src/routes/orderRoutes.js
+++ src/routes/orderRoutes.js
@@ -1,10 +1,24 @@
 import { Router } from 'express'
+import multer from 'multer'
 import { authUser } from '../middlewares/authUser.js'
 import { defineAbilityMiddleware } from '../middlewares/ability.js'
 import { checkAbility } from '../middlewares/checkAbility.js'
-import { getOrders, getOrderById, updateOrderStatus, updateOrder, createOrder } from '../controllers/orderController.js'
+import { BadRequestError } from '../errors/BadRequestError.js'
+import { getOrders, getOrderById, updateOrderStatus, updateOrder, createOrder, uploadPaymentProof } from '../controllers/orderController.js'
 
 const orderRouter = Router()
+const PAYMENT_PROOF_MIMETYPES = ['image/jpeg', 'image/png', 'image/webp', 'application/pdf']
+
+const paymentProofUpload = multer({
+	storage: multer.memoryStorage(),
+	fileFilter: (req, file, cb) => {
+		if (PAYMENT_PROOF_MIMETYPES.includes(file.mimetype)) cb(null, true)
+		else cb(new Error(`File must be one of the following types: ${PAYMENT_PROOF_MIMETYPES.join(', ')}`))
+	},
+	limits: {
+		fileSize: 10 * 1024 * 1024 // 10 MB
+	}
+})
 
 orderRouter.use(authUser, defineAbilityMiddleware)
 
@@ -15,5 +29,16 @@ orderRouter.post('/', checkAbility('create', 'Order'), createOrder)
 
 orderRouter.put('/:orderId/status', checkAbility('update', 'Order'), updateOrderStatus)
 orderRouter.put('/:orderId', checkAbility('update', 'Order'), updateOrder)
+
+orderRouter.post('/:orderId/payment_proof', checkAbility('update', 'Order'), (req, res, next) => {
+	paymentProofUpload.single('image')(req, res, (err) => {
+		if (err) {
+            console.error('Payment proof upload error:', err)
+			return next(new BadRequestError(err.message))
+		}
+
+		uploadPaymentProof(req, res, next)
+	})
+})
 
 export default orderRouter
