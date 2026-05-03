9fef54ee0b4c4949c90e93c1bbbdc07d0b5cd5c3
MarianoNLR <marianonehuenlr@gmail.com>
2026-05-02 23:17:09 -0300

    feat: refactor createPayment function to include payment method and update webhook handling

--- src/controllers/checkoutController.js
+++ src/controllers/checkoutController.js
@@ -1,24 +1,15 @@
-import { MercadoPagoConfig, Payment, Preference } from 'mercadopago'
-import Cart from '../models/Cart.js'
-import 'dotenv/config'
 import * as checkoutService from '../services/checkoutService.js'
-import * as orderService from '../services/orderService.js'
-import Order from '../models/Order.js'
-import { sendOrderEmail } from '../emailController/emailController.js'
 import { sendSuccess } from '../utils/apiResponse.js'
-const { MP_ACCESS_TOKEN } = process.env
 
-const client = new MercadoPagoConfig({
-  accessToken: MP_ACCESS_TOKEN
-})
-
-const payment = new Payment(client)
-
-export async function setPreferences (req, res, next) {
+export async function createPayment(req, res, next) {
   const { userId } = req
-  const { shipping_info } = req.body
+  const { shipping_info, payment_method } = req.body
   try {
-    const result = await checkoutService.setPreferences({userId, shipping_info })
+    const result = await checkoutService.createPayment({
+      userId,
+      shippingInfo: shipping_info,
+      paymentMethod: payment_method
+    })
     return sendSuccess(res, { result }, 200)
   } catch (error) {
     console.error(error)
@@ -27,11 +18,10 @@ export async function setPreferences (req, res, next) {
 }
 
 export const receiveWebhook = async (req, res, next) => {
-  console.log('WEBHOOK: ', req.query)
   const paymentInfo = req.query
-  console.log('PAYMENT INFO: ', paymentInfo)
+  const { provider } = req.params
   try {
-    const result = await checkoutService.receiveWebhook({ paymentInfo })
+    const result = await checkoutService.receiveWebhook({ provider, paymentInfo })
     if (result) {
       return sendSuccess(res, { result }, 201)
     }
